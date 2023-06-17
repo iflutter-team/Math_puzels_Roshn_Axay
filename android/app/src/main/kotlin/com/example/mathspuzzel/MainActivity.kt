2d7b71e11cc067f2ff7c902f2cba0cd071d4cdc6
axay-borada <bordaaxay90@gmail.com>
2023-06-17 11:13:34 +0530

    Git Done

---
+++ android/app/src/main/kotlin/com/example/mathspuzzel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mathspuzzel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+main() {
+  runApp(GetMaterialApp(
+    home: Maths_Puzzel_Game(),
+  ));
+}
