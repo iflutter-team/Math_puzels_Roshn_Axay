bf1e91d0646016b4a9c0091bb82ac94241387e78
Roshan Paladiya <roshanpaladiya2004@gmail.com>
2023-06-17 16:02:29 +0530

    Merge branch 'roshan_dev' of D:\git hub\Math_puzels_Roshn_Axay with conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mathspuzzel/level_screen/level_screen.dart';
 import 'package:mathspuzzel/splash_screen/splash_screen.dart';
 
 main() {
   runApp(
     GetMaterialApp(
-      home: const level_Screen(),
+      home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
